51dd0f827017875636036a3066f9d49977d479bb
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-21 20:05:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,45 @@
 # code here!
+require 'pry'
+
+class School
+#   def website
+#   @website
+# end
+#
+# # Classical set method
+# def website=(website)
+#   @website = website
+# end
+#
+  def initialize(name)
+    @name= name
+    @roster= {}
+  end
+
+  def name
+    @name
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster.empty?
+      @roster[grade] = [name]
+    else
+      @roster.each do |grade_key, name_arr|
+        if grade == grade_key
+          @roster[grade] << name_arr
+        else
+          @roster[grade] = [name]
+        end
+      end
+    end
+
+  end
+
+
+
+
+end
